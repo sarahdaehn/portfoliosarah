bc1b8fed1ce1be48fd2433335861dfc2a64f9529
sarahdaehn <sarahdaehn87@gmail.com>
2024-01-21 20:15:45 +0100

    Adding Links to the Navbar
    Adding Links to the Navbar

--- src/screens/navigationBar/navigationBar.js
+++ src/screens/navigationBar/navigationBar.js
@@ -3,11 +3,25 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import { navigationLinks } from '../../helpers/navigationLinks'
 
+
+function createLinks(){
+    return navigationLinks.map((e,idx) => (
+        <Nav.Link key={idx} href={e.ref}>{e.name}</Nav.Link>
+
+    ))
+}
+
 function navigationBar() {
   return (
     <div id="home">
-      <Navbar>
+      <Navbar expand="md">
       <Navbar.Brand href="#home"> Sarah Daehn</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse>
+        <Nav>
+       {createLinks()}
+       </Nav>
+      </Navbar.Collapse>
       </Navbar>
     </div>
   )
