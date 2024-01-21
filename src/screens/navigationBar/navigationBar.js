e960ab200d0126434374ca278f67f740f6961b8e
sarahdaehn <sarahdaehn87@gmail.com>
2024-01-21 20:01:45 +0100

    added Navbar
    added Navbar

--- src/screens/navigationBar/navigationBar.js
+++ src/screens/navigationBar/navigationBar.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import { navigationLinks } from '../../helpers/navigationLinks'
 
 function navigationBar() {
   return (
-    <div>
-      navigation
+    <div id="home">
+      <Navbar>
+      <Navbar.Brand href="#home"> Sarah Daehn</Navbar.Brand>
+      </Navbar>
     </div>
   )
 }
