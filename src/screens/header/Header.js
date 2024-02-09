2d630d6684d107aa79d614f2eeae435570243229
sarahdaehn <sarahdaehn87@gmail.com>
2024-02-09 16:44:21 +0100

    Added type animation

--- src/screens/header/Header.js
+++ src/screens/header/Header.js
@@ -5,7 +5,27 @@ import { ReactTyped } from 'react-typed'
 function Header() {
   return (
     <div>
-      <h1> What am I</h1>
+
+    <h1>What am I ? </h1>
+    <ReactTyped
+      strings={[
+        " ",
+        "A Software Engineer",
+        "Also known as an",
+        "Front-end developer",
+        "Back-end developer",
+        "Full stack developer",
+        "Or!",
+        " ", 
+        "'a coder'",
+        " ",
+        " "]}
+      typeSpeed={40}
+      backSpeed={50}
+      
+      loop
+    />
+      
     
         
     </div>
